e2065f4654f6dc45359f497e210c59c72cc50249
Jose Linares <linz.webdev@gmail.com>
2025-09-02 05:09:42 -0400

    cambios

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,15 +1,13 @@
-import { Box, darken, lighten, Toolbar, Typography, useTheme } from '@mui/material'
-import { FC, useEffect } from 'react';
+import { AppBar, Box, darken, Dialog, IconButton, lighten, Toolbar, Typography, useTheme } from '@mui/material'
+import React, { FC, useEffect, useState } from 'react';
 import { DescripcionDeVista } from '../components/ui/content/DescripcionDeVista';
 import { Loading } from '../components/ui/content/Loading';
 import { Layout } from '../components/ui/Layout';
 import { useUserStore } from '../store/user/UserStore';
-import { Widget } from '../components/widgets/Widget';
-import Masonry from '@mui/lab/Masonry';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 export const Orders = () => {
     const user = useUserStore(state => state.user);
-    const theme = useTheme();
     const validateToken = useUserStore((state) => state.validateToken);
     const validarSesion = async () => {
         const result = await validateToken();
@@ -24,10 +22,10 @@ export const Orders = () => {
     )
     return (
         <Layout>
-            <DescripcionDeVista title={'Kanban'} description={'Aqui podrás ver las órdenes'} />
-
-            <Box sx={{ display: 'flex', flexFlow: 'row nowrap', overflowX: 'hidden', overflowY: 'hidden', maxwidth: '100%', pr: 5 }}>
+            <DescripcionDeVista title={'Kanban'} description={'example'} />
+            <Box sx={{ display: 'flex', flexFlow: 'row nowrap', overflowX: 'hidden', overflowY: 'hidden', maxWidth: '85%', }}>
                 <Box sx={{
+                    pb: 2,
                     display: 'flex', flexFlow: 'row nowrap',
                     overflowX: 'scroll', overflowY: 'hidden', width: '100%', '&::-webkit-scrollbar': {
                         height: '5px',
@@ -42,51 +40,119 @@ export const Orders = () => {
                         backgroundColor: lighten(user.color, 0.2),
                     },
                 }}>
-                    <Masonry columns={6} spacing={2} sx={{ minWidth: '1200px' }}>
-                        <Widget title='Nuevo' >
-                            <OrderItem title='Orden #1234' customer='Cliente A' price={100} />
-                            <OrderItem title='Orden #1235' customer='Cliente B' price={150} />
-                            <OrderItem title='Orden #1236' customer='Cliente C' price={200} />
-                        </Widget>
-                        <Widget title='En proceso' >
-                            <OrderItem title='Orden #1234' customer='Cliente A' price={100} />
-                        </Widget>
-                        <Widget title='Llamada 1' >
-                            <OrderItem title='Orden #1234' customer='Cliente A' price={100} />
-                            <OrderItem title='Orden #1235' customer='Cliente B' price={150} />
-                            <OrderItem title='Orden #1236' customer='Cliente C' price={200} />
-                        </Widget>
-                        <Widget title='Llamada 2' >
-                            <OrderItem title='Orden #1234' customer='Cliente A' price={100} />
-                            <OrderItem title='Orden #1236' customer='Cliente C' price={200} />
-                        </Widget>
-                        <Widget title='Llamada 3' >
-                            <OrderItem title='Orden #1234' customer='Cliente A' price={100} />
-                        </Widget>
-                        <Widget title='Repartidor' >
-                            <OrderItem title='Orden #1234' customer='Cliente A' price={100} />
-                            <OrderItem title='Orden #1236' customer='Cliente C' price={200} />
-                        </Widget>
-                    </Masonry>
+                    <Box sx={{ display: 'flex', gap: 2, flexFlow: 'row nowrap' }}>
+                        <OrderList title='Nuevo' orders={orders} />
+                        <OrderList title='Asginado a vendedora' orders={orders} />
+                        <OrderList title='Llamado 1' orders={orders} />
+                        <OrderList title='Llamado 2' orders={orders} />
+                        <OrderList title='Llamado 3' orders={orders} />
+                        <OrderList title='Confirmado' orders={orders} />
+                        <OrderList title='Asignado a repartidor' orders={orders} />
+                        <OrderList title='En ruta' orders={orders} />
+                        <OrderList title='Programado para mas tarde' orders={orders} />
+                        <OrderList title='Programado para otro dia' orders={orders} />
+                        <OrderList title='Reprogramado' orders={orders} />
+                        <OrderList title='Cambio de ubicacion' orders={orders} />
+                        <OrderList title='Rechazado' orders={orders} />
+                        <OrderList title='Entregado' orders={orders} />
+                        <OrderList title='Cancelado' orders={orders} />
+                    </Box>
                 </Box>
             </Box>
 
         </Layout>
     )
 }
+const orders = [
+    { id: '1', title: 'Orden #1234', customer: 'Cliente A', price: 100 },
+    { id: '2', title: 'Orden #1235', customer: 'Cliente B', price: 150 },
+    { id: '3', title: 'Orden #1236', customer: 'Cliente C', price: 200 },
+]
+interface OrderListProps {
+    title: string;
+    orders?: any[];
+}
+const OrderList: FC<OrderListProps> = ({ title, orders }) => {
+    const user = useUserStore(state => state.user);
+    return (
+        <Box sx={{ overflow: 'hidden', minHeight: '600px', maxHeight: '600px', gap: 2, background: (theme) => theme.palette.mode === 'dark' ? `${user.color}20` : lighten(user.color, 0.95), border: `1px solid ${user.color}30`, borderRadius: 5, height: 'fit-content' }}>
+            <Box sx={{ background: (theme) => theme.palette.mode === 'dark' ? darken(user.color, 0.8) : 'white', p: 1, boxShadow: '0 8px 16px rgba(150,150,150,0.1)', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Typography variant='h6' sx={{ mb: 2 }}>{title}</Typography>
+            </Box>
+            <Box sx={{ p: 2, gap: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', minWidth: '300px', }}>
+
+                {orders && orders.map((order) => (
+                    <OrderItem key={order.id} id={order.id} title={order.title} customer={order.customer} price={order.price} />
+                ))}
+            </Box>
+        </Box>
+    )
+}
+
 interface OrderItemProps {
+    id?: string;
     title: string;
     customer: string;
     price: number;
 }
-const OrderItem: FC<OrderItemProps> = ({ title, customer, price }) => {
+const OrderItem: FC<OrderItemProps> = ({ id, title, customer, price }) => {
     const user = useUserStore(state => state.user);
-
+    const [open, setOpen] = useState<boolean>(false);
+    const handleOpen = () => setOpen(true);
+    const theme = useTheme();
     return (
-        <Box sx={{ border: `1px solid ${user.color}`, width: '100%', height: '100px', borderRadius: 5, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <Typography>{title}</Typography>
-            <Typography>{customer}</Typography>
-            <Typography>{price}</Typography>
-        </Box>
+        <>
+            <Box onClick={handleOpen} sx={{ cursor: 'pointer', background: theme.palette.mode === 'dark' ? darken(user.color, 0.92) : 'white', boxShadow: `0 8px 16px${user.color}10`, width: '100%', height: '100px', borderRadius: 5, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <Typography>{title}</Typography>
+                <Typography>{customer}</Typography>
+                <Typography>{price}</Typography>
+            </Box>
+            <OrderDialog open={open} setOpen={setOpen} id={id} />
+        </>
+
     )
+}
+interface OrderDialogProps {
+    id?: string;
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const OrderDialog: FC<OrderDialogProps> = ({ id, open, setOpen }) => {
+    const [order, setOrder] = useState<any>({});
+    const user = useUserStore(state => state.user);
+    const theme = useTheme();
+    useEffect(() => {
+        getData();
+    }, []);
+    const handleClose = () => {
+        setOpen(false)
+        console.log(open)
+    };
+    const getData = async () => {
+        const request = await fetch(`/api/orders/${id}`);
+        const data = await request.json();
+        if (data.status) {
+            setOrder(data.order);
+        }
+    }
+    return (
+        <Dialog fullScreen onClose={handleClose} open={open} >
+            <AppBar sx={{ background: theme.palette.mode === 'dark' ? darken(user.color, 0.8) : user.color, p: 2, }} elevation={0}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexFlow: 'row nowrap', width: '100%' }}>
+                    <Typography variant='h6'>Detalle de la orden</Typography>
+                    <IconButton onClick={handleClose}>
+                        <CloseRoundedIcon sx={{ color: (theme) => theme.palette.getContrastText(user.color) }} />
+                    </IconButton>
+                </Box>
+            </AppBar>
+            <Toolbar />
+            <Box sx={{ p: 2, background: theme.palette.mode === 'dark' ? darken(user.color, 0.9) : lighten(user.color, 0.97), minHeight: '100vh', height: '100%' }}>
+                <Typography variant='h5'>Orden #{order.id}</Typography>
+                <Typography>Cliente: {order.customer}</Typography>
+                <Typography>Total: ${order.total}</Typography>
+                <Typography>{open}</Typography>
+            </Box>
+        </Dialog>
+    );
 }
--- src/pages/auth/Profile.tsx
+++ src/pages/auth/Profile.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, CircularProgress } from '@mui/material'
+import { Avatar, Box, CircularProgress, darken, useTheme } from '@mui/material'
 import { DescripcionDeVista } from '../../components/ui/content/DescripcionDeVista'
 import { Layout } from '../../components/ui/Layout'
 import { useUserStore } from '../../store/user/UserStore';
@@ -13,6 +13,7 @@ export const Profile = () => {
     const user = useUserStore(state => state.user);
     const validateToken = useUserStore((state) => state.validateToken);
     const [data, setData] = useState<any>([]);
+    const theme = useTheme();
     const getUserData = async () => {
         const result = await request('/user/data', 'GET');
         console.log({ result });
@@ -40,7 +41,7 @@ export const Profile = () => {
             <DescripcionDeVista title={'Perfil'} description={'Aqui podras ver tus datos personales, editar, entre otras cosas mas!'} />
             <Box sx={{ gap: 2, display: 'flex', flexFlow: 'row wrap' }}>
 
-                <Box sx={{ paddingBlock: 2, border: `2px solid ${user.color}30`, borderRadius: 5, display: 'flex', justifyContent: 'start', alignItems: 'start', flexFlow: 'column wrap', width: '100%' }}>
+                <Box sx={{ paddingBlock: 2, background: theme.palette.mode === 'dark' ? darken(user.color, 0.92) : 'white', borderRadius: 5, display: 'flex', justifyContent: 'start', alignItems: 'start', flexFlow: 'column wrap', width: '100%', boxShadow: `0 8px 16px ${user.color}20` }}>
                     <Box sx={{ display: 'flex', flexFlow: 'column nowrap', justifyContent: 'center', alignItems: 'center', margin: 'auto' }}>
                         <Avatar sx={{ width: 100, height: 100, fontSize: 50, background: user.color, color: (theme) => theme.palette.getContrastText(user.color) }}>{`${user.names.charAt(0)}${user.surnames.charAt(0)}`}</Avatar>
                         <TypographyCustom variant="h6" fontWeight={'bold'}>{`${user.names}`}</TypographyCustom>
@@ -55,7 +56,7 @@ export const Profile = () => {
                         <ThemeChanger />
                     </Box>
                 </Box>
-                <Box sx={{ paddingBlock: 2, border: `2px solid ${user.color}30`, borderRadius: 5, display: 'flex', flexFlow: 'column nowrap', width: '100%' }}>
+                <Box sx={{ paddingBlock: 2, background: theme.palette.mode === 'dark' ? darken(user.color, 0.92) : 'white', boxShadow: `0 8px 16px ${user.color}20`, borderRadius: 5, display: 'flex', flexFlow: 'column nowrap', width: '100%' }}>
                     <Box sx={{ p: 2 }}>
                         <TypographyCustom variant="h6" fontWeight={'bold'}>Datos personales</TypographyCustom>
                         <Box sx={{ display: 'flex', flexFlow: 'column nowrap', gap: 0, pb: 1, pt: 2 }}>
