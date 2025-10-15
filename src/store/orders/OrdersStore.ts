1abfd5619b55cfc6792a55d3c0d2647feb9376a3
Jose Linares <linz.webdev@gmail.com>
2025-10-15 17:07:11 -0400

    cambios

--- src/store/orders/OrdersStore.ts
+++ src/store/orders/OrdersStore.ts
@@ -3,14 +3,17 @@ import { create } from "zustand";
 interface OrdersState {
     orders: any[];
     selectedOrder: any | null;
+    selectedAgentId: number | null;
     setOrders: (orders: any[]) => void;
     updateOrder: (order: any) => void;
     setSelectedOrder: (order: any | null) => void;
+    setSelectedAgentId: (id: number | null) => void;
 }
 
 export const useOrdersStore = create<OrdersState>((set) => ({
     orders: [],
     selectedOrder: null,
+    selectedAgentId: null,
     setOrders: (orders) => set({ orders }),
     updateOrder: (order) =>
         set((state) => ({
@@ -19,4 +22,5 @@ export const useOrdersStore = create<OrdersState>((set) => ({
                 state.selectedOrder?.id === order.id ? order : state.selectedOrder,
         })),
     setSelectedOrder: (order) => set({ selectedOrder: order }),
+    setSelectedAgentId: (id) => set({ selectedAgentId: id }),
 }));
