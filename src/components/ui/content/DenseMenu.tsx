dbccb6f97802db30cfa128295bf87678e0d717e2
Jose Linares <linz.webdev@gmail.com>
2025-11-19 09:33:01 -0400

    dsds

--- src/components/ui/content/DenseMenu.tsx
+++ src/components/ui/content/DenseMenu.tsx
@@ -23,21 +23,21 @@ export default function DenseMenu({
     const user = useUserStore((state) => state.user);
 
     const statuses: { id: number, description: string, color: string, roles: string[] }[] = [
-        { id: 1, description: 'Nuevo', color: purple[300], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 2, description: 'Asignado a vendedora', color: blue[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 3, description: 'Llamado 1', color: green[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 4, description: 'Llamado 2', color: red[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 5, description: 'Llamado 3', color: purple[300], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 6, description: 'Confirmado', color: blue[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 7, description: 'Asignado a repartidor', color: green[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 8, description: 'En ruta', color: red[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 9, description: 'Programado para mas tarde', color: purple[300], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 10, description: 'Programado para otro dia', color: blue[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 11, description: 'Reprogramado', color: green[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 12, description: 'Cambio de ubicacion', color: red[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 13, description: 'Rechazado', color: red[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 14, description: 'Entregado', color: green[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
-        { id: 14, description: 'Cancelado', color: red[500], roles: ['Admin', 'Vendedora', 'Repartidor'] },
+        { id: 1, description: 'Nuevo', color: purple[300], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 2, description: 'Asignado a vendedor', color: blue[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 3, description: 'Llamado 1', color: green[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 4, description: 'Llamado 2', color: red[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 5, description: 'Llamado 3', color: purple[300], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 6, description: 'Confirmado', color: blue[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 7, description: 'Asignado a repartidor', color: green[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 8, description: 'En ruta', color: red[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 9, description: 'Programado para mas tarde', color: purple[300], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 10, description: 'Programado para otro dia', color: blue[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 11, description: 'Reprogramado', color: green[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 12, description: 'Cambio de ubicacion', color: red[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 13, description: 'Rechazado', color: red[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 14, description: 'Entregado', color: green[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
+        { id: 14, description: 'Cancelado', color: red[500], roles: ['Admin', 'Vendedor', 'Repartidor'] },
     ];
     return (
         <>
